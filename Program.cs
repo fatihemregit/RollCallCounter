332248ae18f480777bb229e2e0496c6c14747988
fatih emre <yeni.udemy@gmail.com>
2024-01-18 22:47:11 +0300

    basit bir konsol menüsü tasarlandı

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//main function codes start
+while (true) {
+    Console.WriteLine("===============ANA MENÜ===============\n1-Devamsızlık Ekleme\n2- Ders Bazında Devamsızlık Görüntüleme\n3-Tarih bazında Devamsızlık Görüntüleme");
+    string islemTip = girdiAl("işlem seçiniz");
+    if (islemTip == "1") {
+        //devamsızlık Ekleme
+        Console.WriteLine("Devamsızlık ekleme");
+    }
+    else if (islemTip == "2") {
+        Console.WriteLine("Ders Bazında Devamsızlık Görüntüleme");
+
+    }
+    else if(islemTip == "3")
+    {
+        Console.WriteLine("Tarih bazında Devamsızlık Görüntüleme");
+    }
+    else{
+        Console.WriteLine("Bilinmeyen İşlem!!!");
+    }
+
+}
+//main function codes end
+
+//functions
+string girdiAl(string soruMetni) {
+    Console.WriteLine($"{soruMetni}");
+    string kullaniciGirdisi = Console.ReadLine();
+    return kullaniciGirdisi;
+}
--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 ## Projenin Amacý
 Projenin amacý üniversite ders devamsýzlýklarýnýn kolaylýkla tutulmasýný saðlamak
 ## Bu Committe Yapýlan Ýþlemler
-- Readme Update
+- basit bir konsol menüsü tasarlandý
 ## Proje Günlüðü
-### Gün 1
+### Gün 1 (18.01.2024)
 - Visual Studio Üzerinden Proje Oluþturuldu.
 - Kodlarý bulut sistemler üzerinde tutmak için public þekilde github repository si oluþturuldu.
 - README.md dosyasý ve gitignore dosyasý oluþturuldu
+- basit bir konsol menüsü tasarlandý
 
+<br>
+yapýlacaklar
+
+## kendi notlarým
+öncelikle bir model class oluþtur bir tane de iþlem class ý
